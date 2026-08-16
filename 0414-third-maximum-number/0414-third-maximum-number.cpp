596617c4085bb7565bfa30aff0fabb727b520abf
Manmeet-Singh-Deol <deolmanmeetsingh13@gmail.com>
2026-08-16 21:57:32 +0530

    Time: 0 ms (100%), Space: 12.9 MB (73.54%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long first = LLONG_MIN, second = LLONG_MIN, third = LLONG_MIN;
+
+        for (int num : nums) {
+            if (num == first || num == second || num == third) continue; // skip duplicates
+
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second) {
+                third = second;
+                second = num;
+            } else if (num > third) {
+                third = num;
+            }
+        }
+
+        return third == LLONG_MIN ? first : third;
+    }
+};
